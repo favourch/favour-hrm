7d50887f4e5ed1b9a300d612f77a559b38152470
SenseiFavour <fchukwuedo@gmail.com>
2019-07-18 12:59:38 +0100

    Update footer.php

--- application/views/backend/footer.php
+++ application/views/backend/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="main">
 
-	&copy; 2016 <strong><?php echo $system_name; ?> | Version 1.4</strong> Developed by <a href="http://creativeitem.com" target="_blank">Creativeitem</a>
+	&copy; 2019 <strong><?php echo $system_name; ?> | Version 1.4</strong> Developed by <a href="http://twitter.com/senseifavour" target="_blank">Favour Chukwuedo</a>
 
 </footer>
